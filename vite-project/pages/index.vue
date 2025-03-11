9b1951c9cb19e7796f9aec9b39b796d212efa280
akarapong <97168241+0xAkarapong@users.noreply.github.com>
2025-03-11 22:49:20 +0700

    remove app.vue, add index.vue with header, integrate Tailwind CSS and update nuxt.config.ts

---
+++ vite-project/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <header>
+        <NuxtRouteAnnouncer />
+        <nav class="bg-gray-800 text-white p-4 shadow-lg">
+            <h1 class="text-3xl font-bold">
+            Akarapong Thammawong
+            </h1>
+        </nav>
+    </header>
+</template>
+  
